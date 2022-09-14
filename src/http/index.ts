08d6aff15793cc13d19cf44230ae0f4fcc8090bf
mixaivanov1992 <mixaivanov1992@gmail.com>
2022-09-14 14:29:20 +0300

    dev

--- src/http/index.ts
+++ src/http/index.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const API_URL = 'https://table-party.herokuapp.com'; // 'http://localhost:4001/api';
+export const API_URL = 'http://localhost:4001/api';
 const $api = axios.create({
     withCredentials: true,
     baseURL: API_URL,
